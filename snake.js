5c1153704a405dc20847e1112187abd55e091d0b
Gavin-Slice <126028403+Gavin-Slice@users.noreply.github.com>
2023-03-04 01:43:19 -0600

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,117 +1,45 @@
-//borad
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context;
+/* ================ toggle icon navbar ========= */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-//snake head
-var snakeX = blockSize * 5;
-var snakeY = blockSize *5;
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
-var velocityX = 0;
-var velocityY = 0;
+/* ================ scroll sections avtive link ========= */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-var snakeBody = []
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /* ================ sticky navbar ========= */
+    let header = document.querySelector('header');
 
-//food
-var foodX;
-var foodY;
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-var gameOver = false;
+    /* ================ remove toggle icon  and navbar when click navbar link (scroll) ========= */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
 
-//score
-var score = 0;
-var scoreDisplay = document.getElementById("score");
-
-function updateScore() {
-  score += 1;
-  scoreDisplay.innerHTML = "Score: " + score;
-}
-
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = rows * blockSize;
-    board.width = cols * blockSize;
-    context = board.getContext("2d"); //used for drawing on the board
-
-    scoreDisplay = document.getElementById("score");
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    //update();
-    setInterval(update, 1000/10); //100 millseconds
-      
-}
-
-function update() {
-    if  (gameOver) {
-        return;
-    }
-    context.fillStyle="black";
-    context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle="red";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
-
-    if (snakeX == foodX && snakeY == foodY) {
-        snakeBody.push([foodX, foodY])
-        placeFood();
-        updateScore();
-    }
-
-    for (let i = snakeBody.length-1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i-1];
-    }
-    if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
-    }
-
-    context.fillStyle="lime";
-    snakeX += velocityX * blockSize;
-    snakeY += velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
-    for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
-    }
-
-    //game over conditions
-    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
-        gameOver = true;
-        alert("Game Over");
-    }
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-            gameOver = true;
-            alert("Game Over");
-        }
-    }
-
-}
-
-function changeDirection(e) {
-    if (e.code == "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;      
-    }
-    else if (e.code == "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;      
-    }
-    else if (e.code == "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;      
-    }
-    else if (e.code == "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;      
-    }
-}
-
-function placeFood() {
-    //0-1) *cols -> (0-19.9999) -> (0-19) * 25
-    foodX = Math.floor(Math.random() * cols) * blockSize;
-    foodY = Math.floor(Math.random() * rows) * blockSize;
-}
+ /* ================ scroll reveal ========= */
+ ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
